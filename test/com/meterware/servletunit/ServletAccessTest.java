6af474f929fb6dcd97983d02bac472bf2b5dff8d
vonrosen <hunter.stern@fundingcircle.com>
2018-02-02 09:27:13 -0800

    Add everything

---
+++ test/com/meterware/servletunit/ServletAccessTest.java
@@ -0,0 +1,27 @@
+package com.meterware.servletunit;
+
+/**
+ *
+ * @author <a href="mailto:russgold@httpunit.org">Russell Gold</a>
+ **/
+public class ServletAccessTest extends ServletTestCase {
+
+    public ServletAccessTest( String name ) {
+        super( name );
+    }
+
+
+    public void testServletParameters() throws Exception {
+//        ServletUnitClient client = newClient();
+//        InvocationContext ic = client.newInvocation( "http://localhost/SimpleServlet" );
+//        assertNull( "init parameter 'gender' should be null", ic.getServlet().getServletConfig().getInitParameter( "gender" ) );
+//        assertEquals( "init parameter via config", "red", ic.getServlet().getServletConfig().getInitParameter( "color" ) );
+//        assertEquals( "init parameter directly", "12", ((HttpServlet) ic.getServlet()).getInitParameter( "age" ) );
+//        ic.getServlet().service( ic.getRequest(), ic.getResponse() );
+//
+//        WebResponse wr = client.getResponse( ic );
+//        assertEquals( "Servlet response", "the desired content", wr.getText() );
+    }
+
+
+}
